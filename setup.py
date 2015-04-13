512eedc47c5bdad5d08b4955252ab143999afb70
Olivier Cortès <oc@1flow.io>
2015-04-13 17:41:18 +0200

    update setup and depend on phonenumbers.

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
+""" This is a setup. """
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-VERSION = '0.1.1'
+VERSION = '0.2.1'
 
 setup(
     name='nexmomessage',
     version=VERSION,
     description='A Python wrapper for the Nexmo API',
-    author='Marco Londero',
-    author_email='marco.londero@linux.it',
+    author='Olivier Cortès',
+    author_email='contact@oliviercortes.com',
     license='BSD',
-    url='https://github.com/marcuz/libpynexmo',
+    url='https://github.com/Karmak23/libpynexmo',
     keywords=['Nexmo', 'Python'],
     packages=['nexmomessage'],
+    install_requires=[
+        'phonenumbers',
+    ],
 )
